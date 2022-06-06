0a92d7833320c9124a3fe67c97c4079a1a52b210
kakuttaja <40384624+kakuttaja@users.noreply.github.com>
2022-06-06 19:38:33 +0300

    Added an optimized solution

--- 005.py
+++ 005.py
@@ -1,6 +1,24 @@
-
+from eulerlib.numtheory import Divisors
 
 def main():
+    # Prime factor-optimized method
+    # Multiply all the primes together
+    # with their highest powers.
+
+    d = Divisors()
+    b = 1
+    prime_factors = {}
+    for i in range(2, 21):
+        factors = d.prime_factors(i)
+        for p, q in factors:
+            if p not in prime_factors:
+                prime_factors[p] = q
+            elif prime_factors[p] < q:
+                prime_factors[p] = q
+    for p, q in prime_factors.items():
+        b *= (p**q)
+    return b
+
     # Brute-force method
     limit = 20
     a = 20
@@ -8,8 +26,8 @@ def main():
         for i in range(limit, 0, -1):
             if not a % i == 0:
                 break
-            if i == 1:
-                return a
+        else:
+            return a
         a += 20
 
 if __name__ == '__main__':

a4abcd893c631cac9bffb4fe94068e93c1d3b538
kakuttaja <40384624+kakuttaja@users.noreply.github.com>
2022-06-07 14:01:28 +0300

    Optimized the intervals of n

--- 686.py
+++ 686.py
@@ -3,6 +3,32 @@ from math import log10
 
 def main():
     # Using math formula from https://math.stackexchange.com/a/2006181
+    # 
+    # Updated to consider intervals between powers of 2,
+    # leading to much more efficient iterating by skipping
+    # numbers that can't be correct.
+    # 
+    # The intervals between numbers starting with 123
+    # were just manually checked, seemed to hold through.
+    values = 1
+    target = 678910
+    L = 123
+    n = 1 
+    k = 3
+    intervals = [196, 289, 485]
+    while values < target:
+        for i in intervals:
+            b = int(10**((n+i)*log10(2)-int(1+((n+i)*log10(2)))+k))
+            if b == L:
+                values += 1
+                n += i
+                break
+        else:
+            n += 1
+    return n
+
+def old_main():
+    # This is the original function using the same formula as the new one.
     values = 0
     target = 678910
     L = 123

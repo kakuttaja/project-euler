da635744b2feed88b86c2935f447fc7e32df5b31
kakuttaja <40384624+kakuttaja@users.noreply.github.com>
2022-06-14 14:03:13 +0300

    Added new solutions

---
+++ 070.py
@@ -0,0 +1,67 @@
+import itertools as it
+
+def prime_gen( ):
+    # An optimized Erastosthenes' sieve function
+    # erat2 from https://stackoverflow.com/a/3796442
+    D = {  }
+    yield 2
+    for q in it.islice(it.count(3), 0, None, 2):
+        p = D.pop(q, None)
+        if p is None:
+            D[q*q] = q
+            yield q
+        else:
+            x = q + 2*p
+            while x in D:
+                x += 2*p
+            D[x] = p
+
+def prime_list(num):
+    mypgen = prime_gen()
+    result = []
+    while True:
+        p = next(mypgen)
+        if p > num:
+            break
+        else:
+            result.append(p)
+    return result
+
+def check_digits(a, b) -> bool:
+    return sorted(int(i) for i in str(a)) == sorted(int(i) for i in str(b))
+
+def phi(p1, p2) -> int:
+    return (p1-1)*(p2-1)
+
+def main():
+    # To minimize phi(n), we need to minimise len(prime_factors) and maximise sum(prime_factors)
+    # phi(n) function can also be made very simple, as there is no need to search for factors,
+    # because we are iterating through primes to build a number.
+    # 
+    # To minimise len(prime_factors), only 2 primes are used to build a number.
+    # 
+    # As the primes must both be close to max, we want to limit the length of
+    # primes, but not too much (therefore limit + 10**5), so that they are AROUND sqrt(limit)
+    ans = 0
+    lowest = 99999999
+    limit = 10**7
+    primes = prime_list(int(limit**0.5) + 10**5)
+    a = max(primes)
+    for i in range(len(primes) - 1):
+        for j in range(i + 1, len(primes)):
+            p1 = primes[i]
+            p2 = primes[j]
+            if p1*p2 > limit: break
+            n = int(phi(p1, p2))
+            if check_digits(p1*p2, n):
+                if (p1*p2)/n < lowest:
+                    lowest = (p1*p2)/n
+                    ans = p1*p2
+                    # print(">", p1, p2, p1*p2)
+    return ans
+
+if __name__ == '__main__':
+    import time
+    start = time.time()
+    print(main())
+    print(f"Time taken: {round(time.time() - start, 2)}s")

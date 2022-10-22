3c2731478e59d16b5402c98f539f4b698e3c0f77
kakuttaja <40384624+kakuttaja@users.noreply.github.com>
2022-10-22 16:21:20 +0300

    Added lru_caching

--- 014.py
+++ 014.py
@@ -1,30 +1,30 @@
+import functools
 
-
-RESULTS = {}
-
-def collatz(num: int):
-    i = 1
-    while num != 1:
-        if num % 2 == 0:
-            num /= 2
-        else:
-            num = 3*num + 1
-        if num in RESULTS.keys():
-            return RESULTS[num] + i
-        i += 1
-    return i
-
-def main():
+def main() -> int:
+    # Added lru_cache from functools to improve performance
+    # by caching previously found values
+    # 
+    # Too lazy to make a dynamic dictionary for the same purpose.
     ans = 0
-    num = 0
-    for i in range(1, 1*10**6):
-        n = collatz(i)
-        if i not in RESULTS.keys():
-            RESULTS[i] = n
-        if n > ans:
-            num = i
-            ans = n
-    return num
+    length = 0
+
+    @functools.lru_cache(None)
+    def collatz(num: int) -> int:
+        if num == 1: return 1
+        if num % 2 == 0: num = num // 2
+        else: num = 3 * num + 1
+        return collatz(num) + 1
+    
+    for i in range(1, 10**6):
+        result = collatz(i)
+        if result > length:
+            length = result
+            ans = i
+    return ans
+
 
 if __name__ == '__main__':
+    from time import perf_counter
+    start = perf_counter()
     print(main())
+    print(f"This took {round(perf_counter() - start, 2)}s")
